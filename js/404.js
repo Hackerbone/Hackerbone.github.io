ac60cddfd6668212bf5989dc803bd15a6172f26b
Hackerbone <sitaraman.raghav.india@gmail.com>
2018-11-13 23:02:43 +0530

    Clean Up

--- js/404.js
+++
@@ -1,7 +0,0 @@
-var animation = lottie.loadAnimation({
-    container: document.getElementById('404'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'lottie/404_page.json'
-  })
