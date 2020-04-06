8d816dff4cec4149a234c94ef12f4feda3a0817f
Sitaraman S <sitaraman.raghav.india@gmail.com>
2020-04-06 17:27:33 +0530

    Old commit

---
+++ oldie/Scripts/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+   $(".name").fadeOut(0);
+   $(".username").fadeOut(0);
+     setTimeout(function(){
+       $(".name").slideDown(500);
+       $(".username").slideDown();}, 950);
+
+ });
