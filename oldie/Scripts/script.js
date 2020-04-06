f2e5385fd2c40f1247129515242bbe2d7eaef09c
Sitaraman S <sitaraman.raghav.india@gmail.com>
2020-04-06 17:39:20 +0530

    Delete script.js

--- oldie/Scripts/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-   $(".name").fadeOut(0);
-   $(".username").fadeOut(0);
-     setTimeout(function(){
-       $(".name").slideDown(500);
-       $(".username").slideDown();}, 950);
-
- });
