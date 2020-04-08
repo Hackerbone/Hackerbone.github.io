e3e8e0848ab29f0d527e2ea01670c8a5d3c3f03c
Sitaraman S <sitaraman.raghav.india@gmail.com>
2020-04-08 10:40:36 +0530

    Update script.js

--- old-stuff/Scripts/script.js
+++ old-stuff/Scripts/script.js
@@ -1,9 +1,13 @@
 
+
 $(document).ready(function(){
   $(".name").fadeOut(0);
   $(".username").fadeOut(0);
+  $(".github-logo").fadeOut(0);
     setTimeout(function(){
       $(".name").slideDown(500);
-      $(".username").slideDown();}, 950);
+      $(".username").slideDown();
+      $(".github-logo").fadeIn(600);
 
+    }, 950);
 });
